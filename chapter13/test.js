ccb58fe20b19aba77bf4e5c78f5fce43f025ccd3
Shian <shianhuang2008@gmail.com>
2014-12-04 22:49:57 -0600

    finished EventUtil function

--- chapter13/test.js
+++ chapter13/test.js
@@ -12,6 +12,23 @@ var EventUtil = {
 		}
 	},
 
+	getEvent: function(event) {
+		return event? event : window.event;
+	},
+
+	getTarget: function(event) {
+		return event.target || event.srcElement;
+	},
+
+	preventDefault: function(event) {
+		if(event.preventDefault) {
+			event.preventDefault();
+		} 
+		else {
+			event.returnValue = false;
+		}
+	}, 
+
 	removeHandler: function(element, type, handler) {
 		if(element.removeEventListener) {
 			element.removeEventListener(type, handler, false);
@@ -22,6 +39,15 @@ var EventUtil = {
 		else {
 			element["on" + type] = null;
 		}
+	},
+
+	stopPropagation: function(event) {
+		if(event.stopPropagation) {
+			event.stopPropagation();
+		}
+		else {
+			event.cancelBubble = true;
+		}
 	}
 }
 
@@ -30,12 +56,35 @@ var handler = function() {
 	alert("Clicked");
 }
 
-btn.onclick = function(e) {
-	alert(e.type);
+btn.onclick = function(event) {
+	// alert(event.eventPhase + "btn.onclick");
+	event = EventUtil.getEvent(event);
+	var target = EventUtil.getTarget(event);
+	// EventUtil.stopPropagation(event);
+	event.stopPropagation();
+	alert("target " + target);
 }
 
-var myLink = document.getElementById("myLink");
-myLink.onclick = function(event) {
-	// alert(event.type);
-	return false;
+// document.body.addEventListener("click", function(event) {
+// 	alert(event.eventPhase + "document.body.addEventListener");
+// }, true);
+
+document.body.onclick = function(event) {
+	alert("document.body.onclick");
 }
+
+
+// btn.onclick = function(e) {
+// 	alert("Clicked");
+// 	e.stopPropagation();
+// }
+
+// document.body.onclick = function(event) {
+// 	alert("Body clicked");
+// }
+
+// var myLink = document.getElementById("myLink");
+// myLink.onclick = function(event) {
+// 	event = EventUtil.getEvent(event);
+// 	EventUtil.preventDefault(event);
+// }
